a92c1af4a23fc76fb080f5bb1d5d72927dab5dec
E-Hizzle <eho.rutgers@gmail.com>
2016-05-17 13:13:09 -0400

    Added loading page.

--- assets/js/spa.js
+++ assets/js/spa.js
@@ -1,5 +1,11 @@
 // Successfully connected.
 
+// Loading screen
+window.addEventListener("load", function(){
+  var load_screen = document.getElementById("load_screen");
+  document.body.removeChild(load_screen);
+});
+
 $(".loginDiv").hide();
 $(".createDiv").hide();
 
